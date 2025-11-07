07d6fe54e7ca7206381615d1d01b88a418cf852b
kaushik-yadav <kaushiky005@gmail.com>
2025-11-07 22:29:05 +0530

    refactor: Code Clean-up

--- rag_system/image_processor.py
+++
@@ -1,8 +0,0 @@
-from constants import GROQ_API_KEY
-from rag_system.groq_processor import get_image_caption, init_groq
-
-# Initialize Groq processor
-init_groq(GROQ_API_KEY)
-
-# The get_image_caption function is now imported from groq_processor
-# This file maintains compatibility with existing code
